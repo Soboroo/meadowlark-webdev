5c2e72b04b458e4f62e7f0e1789b9e91c090e596
Woo Yeongyun <woo036325@gmail.com>
2022-04-01 17:51:39 +0900

    send querystring, cookie, session value to view

--- meadowlark.js
+++ meadowlark.js
@@ -1,6 +1,7 @@
 const handlers = require('./lib/handlers');
 const express = require('express');
 const { engine } = require('express-handlebars');
+const req = require('express/lib/request');
 const app = express();
 const port = process.env.port || 3000;
 
@@ -12,9 +13,19 @@ app.get('/', handlers.home);
 
 app.get('/about', handlers.about);
 
+app.get('/greeting', (req, res) => {
+	res.render('greeting', {
+		message: 'Hello esteemed programmer!',
+		style: req.query.style,
+		//userid: req.cookies.userid,
+		//username: req.session.username
+	})
+});
+
 app.use(handlers.notFound);
 
 app.use(handlers.serverError);
+
 
 if (require.main === module) {
 	app.listen(port, () => {
